c26f4243317c1563b818dd5146259a3b3cddf32e
Thien <tranhangocthien@gmail.com>
2017-04-30 16:17:12 +0700

    commit by Thien

---
+++ src/java/Servlet/CommonQuestion/Answer.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlet.CommonQuestion;
+
+import Model.JSONPaser.Test2JSON;
+import Model.Test;
+import Model.TestData;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author tutyb
+ */
+@WebServlet(name = "Answer", urlPatterns = {"/Answer"})
+public class Answer extends HttpServlet {
+
+    String DATA_PATH = getServletContext().getRealPath("/") + "Data/";
+    String modelPath = DATA_PATH + "model";
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json;charset=UTF-8");
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json; charset=UTF-8");
+
+        String action = request.getParameter("action");
+
+        if (action.equals("getAnswers")) {
+            getAnswers(request, response);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    private void getAnswers(HttpServletRequest request, HttpServletResponse response) 
+            throws UnsupportedEncodingException {
+        String question = URLDecoder.decode(request.getParameter("question"), "UTF-8");
+    }
+
+}
--- src/java/lemurproject/indri/indri.java
+++
@@ -1,9 +0,0 @@
-package lemurproject.indri;
-
-public class indri {}
-
-
-/* Location:              E:\thien\Learning\NLP\Project\IndexUI.jar!\lemurproject\indri\indri.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       0.7.1
- */
